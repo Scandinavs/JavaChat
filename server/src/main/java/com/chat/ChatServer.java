eedc352f4ff80fd31ba9cb5707c4c0f5d3e95ca3
Scandinavs <scandinavs@gmail.com>
2013-03-11 15:29:51 +0200

    Added log4j and properties file.

--- server/src/main/java/com/chat/ChatServer.java
+++ server/src/main/java/com/chat/ChatServer.java
@@ -7,28 +7,37 @@ import com.chat.messagesender.MessageSender;
 import com.chat.model.DataHolder;
 import com.chat.model.User;
 import org.apache.commons.io.IOUtils;
+import org.apache.log4j.Logger;
 
 import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.net.ServerSocket;
 import java.net.Socket;
-import java.util.logging.Level;
-import java.util.logging.Logger;
+import java.util.Properties;
 
 
 public class ChatServer {
 
-    private static final Logger logger = Logger.getLogger("ChatServer");
+    private static final Logger logger = Logger.getLogger(ChatServer.class);
 
     public static void main(String[] args) {
+        final InputStream inputStream = ChatServer.class.getClassLoader().getResourceAsStream("server.properties");
+        Properties properties = new Properties();
+        try {
+            properties.load(inputStream);
+        } catch (IOException e) {
+            logger.error("Error reading properties file.");
+            System.exit(-1);
+        }
         ServerSocket serverSocket = null;
         boolean listening = true;
 
         try {
             serverSocket = new ServerSocket(4444);
         } catch (IOException e) {
-            logger.warning("Could not listen on port: 4444.");
+            logger.error("Could not listen on port: 4444.");
             System.exit(-1);
         }
 
@@ -48,7 +57,7 @@ public class ChatServer {
             DataHolder.addConnection(connection);
             new ServerThread(connection).start();
         } catch (IOException e) {
-            logger.log(Level.WARNING, "Error creating connection!", e);
+            logger.error("Error creating connection!", e);
             IOUtils.closeQuietly(socket);
         }
     }
